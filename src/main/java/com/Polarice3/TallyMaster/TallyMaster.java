8a590ebd60ad02a2723e627ac9cc7f246d49a783
Polarice3 <zarularief04@gmail.com>
2024-01-30 22:18:46 +0800

    Custom Advancements

--- src/main/java/com/Polarice3/TallyMaster/TallyMaster.java
+++ src/main/java/com/Polarice3/TallyMaster/TallyMaster.java
@@ -2,8 +2,10 @@ package com.Polarice3.TallyMaster;
 
 import com.Polarice3.TallyMaster.client.ClientProxy;
 import com.Polarice3.TallyMaster.common.CommonProxy;
+import com.Polarice3.TallyMaster.common.advancements.TallyCriteriaTriggers;
 import com.Polarice3.TallyMaster.common.network.ModNetwork;
 import com.Polarice3.TallyMaster.init.ModProxy;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
@@ -18,6 +20,10 @@ public class TallyMaster {
     public static final String MOD_ID = "tally_master";
     public static ModProxy PROXY = DistExecutor.unsafeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
 
+    public static ResourceLocation location(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+
     public TallyMaster()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -27,6 +33,8 @@ public class TallyMaster {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, TallyConfig.SPEC);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        TallyCriteriaTriggers.init();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
