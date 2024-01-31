657ff557004e3820336595f3f00eed83c7ba9d6d
Polarice3 <zarularief04@gmail.com>
2024-01-31 18:31:04 +0800

    Updated more stuff

--- src/main/java/com/Polarice3/TallyMaster/TallyConfig.java
+++ src/main/java/com/Polarice3/TallyMaster/TallyConfig.java
@@ -13,12 +13,18 @@ public class TallyConfig {
             .comment("Set the amount of kills to reach a milestone.")
             .defineInRange("tallyMilestone", 25, 1, Integer.MAX_VALUE);
     private static final ForgeConfigSpec.IntValue TALLY_LOOTING_AMOUNT = BUILDER
-            .comment("Only works if tallyLooting is enabled. Defines how many times the tallyMilestone has to be reached to increase looting.")
-            .defineInRange("tallyLootingAmount", 4, 1, Integer.MAX_VALUE);
+            .comment("Only works if tallyLooting is enabled. Defines the amount of kills to increase looting level. Best if value is higher than tallyMilestone.")
+            .defineInRange("tallyLootingAmount", 100, 1, Integer.MAX_VALUE);
     private static final ForgeConfigSpec.IntValue TALLY_MAX_LOOTING_LEVEL = BUILDER
             .comment("Only works if tallyLooting is enabled. The maximum looting level the player can attain from looting milestones.")
             .defineInRange("tallyMaxLootingLevel", 3, 1, Integer.MAX_VALUE);
 
+    private static final ForgeConfigSpec.BooleanValue TALLY_DAMAGE = BUILDER
+            .comment("Enables increase damage against tallied mobs after reaching milestones.")
+            .define("tallyDamage", true);
+    private static final ForgeConfigSpec.BooleanValue TALLY_RESISTANCE = BUILDER
+            .comment("Enables increase resistance against tallied mobs after reaching milestones.")
+            .define("tallyResistance", true);
     private static final ForgeConfigSpec.BooleanValue PET_TALLY = BUILDER
             .comment("Whether kills by pets or owned mobs count to player's tally.")
             .define("petTally", true);
@@ -32,6 +38,8 @@ public class TallyConfig {
     public static int tallyLootingAmount;
     public static int tallyMaxLootingLevel;
 
+    public static boolean tallyDamage;
+    public static boolean tallyResistance;
     public static boolean petTally;
     public static boolean tallyLooting;
 
@@ -41,6 +49,8 @@ public class TallyConfig {
         tallyLootingAmount = TALLY_LOOTING_AMOUNT.get();
         tallyMaxLootingLevel = TALLY_MAX_LOOTING_LEVEL.get();
 
+        tallyDamage = TALLY_DAMAGE.get();
+        tallyResistance = TALLY_RESISTANCE.get();
         petTally = PET_TALLY.get();
         tallyLooting = TALLY_LOOTING.get();
     }
